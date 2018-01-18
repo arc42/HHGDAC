2d5d3d5719ff4be81367882ac558d8adbdb84125
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2018-01-18 21:57:27 +0100

    folge 6

---
+++ folge-6/Config.groovy
@@ -0,0 +1,20 @@
+
+// Path where the docToolchain will produce the output files.
+// This path is appended to the docDir property specified in gradle.properties
+// or in the command line, and therefore must be relative to it.
+outputPath = 'build/docs'
+
+// Path where the docToolchain will search for the input files.
+// This path is appended to the docDir property specified in gradle.properties
+// or in the command line, and therefore must be relative to it.
+inputPath = 'src/docs'
+
+inputFiles = [[file: 'test.adoc',              formats: ['html','pdf','docbook']],
+              [file: 'manual.adoc',            formats: ['html']],
+              [file: 'ppt/Demo.pptx.ad',       formats: ['html','revealjs']],
+             ]
+
+taskInputsDirs = ["${inputPath}/images",
+                 ]
+
+taskInputsFiles = []
