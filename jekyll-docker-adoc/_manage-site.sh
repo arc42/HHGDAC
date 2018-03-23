533cc38630de8833ca55673aff7112794fbfbc66
GernotStarke@MadZorg42 <gs@gernotstarke.de>
2018-03-23 23:05:23 +0100

    updates in config, compose and footer

--- jekyll-docker-adoc/_manage-site.sh
+++ jekyll-docker-adoc/_manage-site.sh
@@ -2,16 +2,22 @@
 #
 # helper, so you don't need to remember docker-compose syntax...
 
+# what's the site?
+site="hhtdac.org"
+server="some.nice-server.de"
+localdir="zz-production-site"
+
 # some colors to highlight certain output
 GREEN=`tput setaf 2`
 RED=`tput setaf 5`
+BLUE=`tput setaf 6`
 RESET=`tput sgr0`
 
 clear
 
 echo
-echo "Docker container to develop or build this website:"
-echo "=================================================="
+echo "Docker container to develop or build the ${BLUE}$site ${RESET}website:"
+echo "============================================================"
 echo
 echo "Please select wether to ${GREEN}develop ${RESET} or ${RED} build ${RESET} the site:"
 echo
@@ -21,7 +27,10 @@ echo
 echo "${GREEN}(b)build ${RESET} build the required docker image."
 echo
 echo "${RED}(p)production ${RESET} produces the site with production configuration,"
-echo "into ./zz-site directory."
+echo "into ./zz-production-site directory."
+echo
+echo "${GREEN}(u)pload ${RESET} the generated site to $server."
+echo
 echo "=================================================="
 echo
 
@@ -41,9 +50,15 @@ case "$choice" in
                    docker-compose --file _docker-compose-dev.yml up
                    ;;
 
-  p|P|production)       echo "create production site"
+  p|P|production)  echo "create production site"
                    docker-compose --file _docker-compose-prod.yml up
                    docker-compose --file _docker-compose-prod.yml down
+                   ;;
+
+  u|U|upload)     echo "upload generated site to server"
+                  docker run -it gernotstarke/ftp-uploader:0.2 \
+                    -v $localdir:/$localdir \
+                    $site $server $localdir
                    ;;
 
   # catchall: abort
